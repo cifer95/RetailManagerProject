d100fd9c0cffafc18786f21eaffaa1630758639e
Michael Sheehan <mks85311@gmail.com>
2022-05-12 13:30:50 -0500

    WPF App: Login Form Error Handling

--- RMPDesktopUI/ViewModels/LoginViewModel.cs
+++ RMPDesktopUI/ViewModels/LoginViewModel.cs
@@ -38,6 +38,39 @@ namespace RMPDesktopUI.ViewModels
             }
         }
 
+        private bool _isErrorVisible;
+
+        public bool IsErrorVisible
+        {
+            get
+            {
+                bool output = false;
+
+                if (ErrorMessage?.Length > 0)
+                {
+                    output = true;
+                }
+
+                return output;
+            }
+
+        }
+
+        private string _errorMessage;
+
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                NotifyOfPropertyChange(() => IsErrorVisible);
+                NotifyOfPropertyChange(() => ErrorMessage);
+            }
+        }
+
+
+
         public bool CanLogIn
         {
             get
@@ -57,12 +90,13 @@ namespace RMPDesktopUI.ViewModels
         {
             try
             {
+                ErrorMessage = "";
                 var result = await _apiHelper.Authenticate(UserName, Password);
             }
             catch (Exception ex)
             {
 
-                Console.WriteLine(ex.Message);
+                ErrorMessage = ex.Message;
             }
         }
 
