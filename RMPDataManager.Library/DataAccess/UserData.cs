5ccc2f5fc26e221b8f70f4865e894c0a4af0576e
Michael Sheehan <mks85311@gmail.com>
2022-05-12 19:15:29 -0500

    Getting User Data part 1

---
+++ RMPDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using RMPDataManager.Library.Internal.DataAccess;
+using RMPDataManager.Library.Models;
+
+namespace RMPDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "RMPData");
+
+            return output;
+        }
+    }
+}
---
+++ RMPDataManager/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+using RMPDataManager.Library.DataAccess;
+using RMPDataManager.Library.Models;
+
+namespace RMPDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+    }
+}
